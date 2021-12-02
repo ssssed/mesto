cbbebdda2419cdc7480f3884e7b64f9518d863c3
sed <igorkoneshov@mail.ru>
2021-12-02 15:47:04 +0400

    Пофиксил все недочеты после 1 ревью

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,25 +3,29 @@ const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".modal__close-btn");
 const save = document.querySelector(".modal__save");
 const like = document.querySelectorAll(".card__like");
+let newName = document.getElementById("name");
+let profileName = document.querySelector(".profile__name");
+let newJob = document.getElementById("job");
+let profileJob = document.querySelector(".profile__job");
+
 
 function openEdit() {
-    modal.classList.add("modal_active")
+    modal.classList.add("modal_active");
+    newName.value = profileName.textContent;
+    newJob.value = profileJob.textContent;
 }
 
 function closeEdit() {
     modal.classList.remove("modal_active");
 }
 
-function profileRename() {
-    let newName = document.getElementById("name").value;
-    let profileName = document.querySelector(".profile__name");
-    let newJob = document.getElementById("job").value;
-    let profileJob = document.querySelector(".profile__job");
-    profileName.textContent = newName;
-    profileJob.textContent = newJob;
+function profileRename(evt) {
+    evt.preventDefault();
+    profileName.textContent = newName.value;
+    profileJob.textContent = newJob.value;
     closeEdit();
 }
 
 anchor.addEventListener("click", openEdit);
 closeBtn.addEventListener("click", closeEdit);
-save.addEventListener("click", profileRename);
+modal.addEventListener("submit", profileRename);
