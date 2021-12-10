81ac819e8d53bf6599b2d69eaf2cdaf1542deede
sed <igorkoneshov@mail.ru>
2021-12-10 19:34:25 +0400

    WIP: Создал popup для кнопки add и добавил кнопки удаления карточек(не работают еще). Переделал открытие и закрытие модалок

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,44 @@
 const anchor = document.querySelector(".profile__edit");
 const modal = document.querySelector(".modal");
+const modalAdd = document.querySelector(".modaladd");
 const form = document.querySelector(".modal__inner");
-const closeBtn = document.querySelector(".modal__close-btn");
+const closeEditBtn = document.querySelector(".modal__close-btn");
+const closeAddBtn = document.querySelector(".modaladd__close-btn");
 const save = document.querySelector(".modal__save");
 const like = document.querySelectorAll(".card__like");
+const add = document.querySelector(".profile__add");
 let newName = document.getElementById("name");
 let profileName = document.querySelector(".profile__name");
 let newJob = document.getElementById("job");
 let profileJob = document.querySelector(".profile__job");
 
 
-function openEdit() {
-    modal.classList.add("modal_active");
-    newName.value = profileName.textContent;
-    newJob.value = profileJob.textContent;
+function open(popup) {
+    if (popup.target.classList.value === "profile__edit") {
+      modal.classList.add("modal_active");
+      newName.value = profileName.textContent;
+      newJob.value = profileJob.textContent;
+    }
+    
+    if (popup.target.classList.value === "profile__add") {
+        modalAdd.classList.add("modal_active");
+    }
 }
 
-function closeEdit() {
-    modal.classList.remove("modal_active");
+function close(popup) {
+    if (popup.target.classList.value === "modal__close-btn") {
+      modal.classList.remove("modal_active");
+    }
+    
+    if (popup.target.classList.value === "modaladd__close-btn") {
+        modalAdd.classList.remove("modal_active");
+    }
 }
+
+function openImg(img) {
+    
+}
+
 
 function profileRename(evt) {
     evt.preventDefault();
@@ -37,6 +57,8 @@ like.forEach((btn) => {
     })
 })
 
-anchor.addEventListener("click", openEdit);
-closeBtn.addEventListener("click", closeEdit);
+anchor.addEventListener("click", open);
+add.addEventListener("click", open)
+closeEditBtn.addEventListener("click", close);
+closeAddBtn.addEventListener("click", close);
 form.addEventListener("submit", profileRename);
