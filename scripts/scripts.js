aabf2233b8563cebda377cae4bb73be8be7799b0
sed <igorkoneshov@mail.ru>
2021-12-13 17:48:32 +0400

    Finished: Оптимизировал JS код на будущее, создав универсальные функции. А также переименовал переменные для удобства. Также переименовал пару классов в html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,131 +1,153 @@
-const anchor = document.querySelector(".profile__edit");
-const modal = document.querySelector(".modal");
-const modalAdd = document.querySelector(".modaladd");
+const editBtn = document.querySelector(".profile__edit");
+const modalEditProfile = document.querySelector(".modal-edit");
+const modalAddCard = document.querySelector(".modal-add");
 const form = document.querySelector(".modal__inner");
-const closeEditBtn = document.querySelector(".modal__close-btn");
-const closeAddBtn = document.querySelector(".modaladd__close-btn");
-const save = document.querySelector(".modal__save");
-const add = document.querySelector(".profile__add");
+const closeEditBtn = document.querySelector(".modal-edit__close-btn");
+const closeAddBtn = document.querySelector(".modal-add__close-btn");
+const addBtn = document.querySelector(".profile__add");
 const addInputName = document.getElementById("placename");
 const addInputLink = document.getElementById("imglink");
-const addForm = document.querySelector(".modaladd__inner");
+const addForm = document.querySelector(".modal-add__inner");
 const template = document.querySelector(".template");
-const list = document.querySelector(".elements");
-const openCard = document.querySelector(".opencard");
+const listCards = document.querySelector(".elements");
+const modalOpenCard = document.querySelector(".opencard");
 const closeCardBtn = document.querySelector(".opencard__close-btn");
-const newCard = document.querySelector(".opencard__img");
-const newCardName = document.querySelector(".opencard__title");
+const modalCardImg = document.querySelector(".opencard__img");
+const modalCardName = document.querySelector(".opencard__title");
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-]; 
-let newName = document.getElementById("name");
-let profileName = document.querySelector(".profile__name");
-let newJob = document.getElementById("job");
-let profileJob = document.querySelector(".profile__job");
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
 
-anchor.addEventListener("click", (evt) => {
-  modal.classList.add("modal_active");
+const newName = document.getElementById("name");
+const profileName = document.querySelector(".profile__name");
+const newJob = document.getElementById("job");
+const profileJob = document.querySelector(".profile__job");
+
+editBtn.addEventListener("click", (evt) => {
+  openPopup(modalEditProfile);
   newName.value = profileName.textContent;
   newJob.value = profileJob.textContent;
-})
+});
 
-add.addEventListener("click", (evt) => {
-  modalAdd.classList.add("modal_active");
-})
+addBtn.addEventListener("click", (evt) => {
+  openPopup(modalAddCard);
+});
 
 closeEditBtn.addEventListener("click", (evt) => {
-  modal.classList.remove("modal_active");
-})
+  closePopup(modalEditProfile);
+});
 
 closeAddBtn.addEventListener("click", (evt) => {
-  modalAdd.classList.remove("modal_active");
-})
+  closePopup(modalAddCard);
+});
 
 form.addEventListener("submit", (evt) => {
-  modal.classList.remove("modal_active");
-})
+  closePopup(modalEditProfile);
+});
 
 addForm.addEventListener("submit", (evt) => {
-  modalAdd.classList.remove("modal_active");
-})
+  closePopup(modalAddCard);
+});
 
-function initialCard() {
-    for (let i = 0; i < initialCards.length; i++) {
-        const newItem = template.content.cloneNode(true).querySelector(".card");
-        newItem.querySelector(".card__title").textContent = initialCards[i].name;
-        newItem.querySelector(".card__img").alt = initialCards[i].name;
-        newItem.querySelector(".card__img").src = initialCards[i].link;
-        newItem.querySelector(".card__trash").addEventListener("click", (evt) => {
-          evt.target.parentElement.remove();
-        });
-        newItem.querySelector(".card__like").addEventListener("click", (evt) => {
-          evt.target.classList.toggle("card__like_active");
-        });
-        newItem.querySelector(".card__img").addEventListener("click", (evt) => {
-          openCard.classList.add("modal_active");
-          newCard.src = initialCards[i].link;
-          newCard.alt = initialCards[i].name;
-          newCardName.textContent = initialCards[i].name;
-        })
-        list.append(newItem);
-    }
+function openPopup(popup) {
+  popup.classList.add("modal_active");
 }
 
-function profileRename(evt) {
-    evt.preventDefault();
-    profileName.textContent = newName.value;
-    profileJob.textContent = newJob.value;
+function closePopup(popup) {
+  popup.classList.remove("modal_active");
 }
 
-addForm.addEventListener("submit", e => {
-    e.preventDefault();
+function createCard(item, title, address) {
+  item.querySelector(".card__title").textContent = title;
+  item.querySelector(".card__img").alt = title;
+  item.querySelector(".card__img").src = address;
+  return item;
+}
 
+function renderCard(list, item, title, address) {
+  createCard(item, title, address);
+  return list.append(item);
+}
+
+function openCardPopup(name, link) {
+    openPopup(modalOpenCard);
+    modalCardImg.src = link;
+    modalCardImg.alt = name;
+    modalCardName.textContent = name;
+}
+
+function loadingCards() {
+  for (let i = 0; i < initialCards.length; i++) {
     const newItem = template.content.cloneNode(true).querySelector(".card");
-    newItem.querySelector(".card__title").textContent = addInputName.value;
-    newItem.querySelector(".card__img").alt = addInputName.value;
-    newItem.querySelector(".card__img").src = addInputLink.value;
+    createCard(newItem, initialCards[i].name, initialCards[i].link);
     newItem.querySelector(".card__trash").addEventListener("click", (evt) => {
-      evt.target.parentElement.remove();
+      evt.target.closest(".card").remove();
     });
     newItem.querySelector(".card__like").addEventListener("click", (evt) => {
       evt.target.classList.toggle("card__like_active");
     });
     newItem.querySelector(".card__img").addEventListener("click", (evt) => {
-      openCard.classList.add("modal_active");
-      newCard.src = newItem.querySelector(".card__img").src;
-      newCard.alt = addInputName.value;
-      newCardName.textContent = addInputName.value;
-    })
-    list.append(newItem);
-})
+      openCardPopup(initialCards[i].name, initialCards[i].link);
+    });
+    renderCard(listCards, newItem, initialCards[i].name, initialCards[i].link);
+  }
+}
 
-initialCard();
+function profileRename(evt) {
+  evt.preventDefault();
+  profileName.textContent = newName.value;
+  profileJob.textContent = newJob.value;
+  closePopup(modalEditProfile);
+}
+
+addForm.addEventListener("submit", (e) => {
+  
+  e.preventDefault();
+
+  const newItem = template.content.cloneNode(true).querySelector(".card");
+  createCard(newItem, addInputName.value, addInputLink.value);
+  newItem.querySelector(".card__trash").addEventListener("click", (evt) => {
+    evt.target.closest(".card").remove();
+  });
+
+  newItem.querySelector(".card__like").addEventListener("click", (evt) => {
+    evt.target.classList.toggle("card__like_active");
+  });
+
+  newItem.querySelector(".card__img").addEventListener("click", (evt) => {
+    openCardPopup(addInputName.value, newItem.querySelector(".card__img").src)
+  });
+
+  renderCard(listCards, newItem, addInputName.value, addInputLink.value);
+});
+
+loadingCards();
 
 form.addEventListener("submit", profileRename);
 
 closeCardBtn.addEventListener("click", (evt) => {
-  openCard.classList.remove("modal_active");
-})
+  modalOpenCard.classList.remove("modal_active");
+});
