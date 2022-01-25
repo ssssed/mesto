1476cc602ac437d1db5ec05ba10023d476410288
sed <igorkoneshov@mail.ru>
2022-01-25 18:24:27 +0400

    ready refactoring validation and appending card

---
+++ scripts/FormValidator.js
@@ -0,0 +1,74 @@
+class FormValidator {
+  constructor(data, form) {
+    this._inputSelector = data.inputSelector;
+    this._submitButtonSelector = data.submitButtonSelector;
+    this._inactiveButtonClass = data.inactiveButtonClass;
+    this._inputErrorClass = data.inputErrorClass;
+    this._form = form;
+  }
+
+  enableValidation() {
+    this._setEventListeners();
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._buttonElement.classList.add(this._inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._inactiveButtonClass);
+      this._buttonElement.disabled = false;
+    }
+  }
+
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  _showInputError(inputElement) {
+    this._errorElement = this._form.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.add(this._inputErrorClass);
+    this._errorElement.textContent = inputElement.validationMessage;
+  }
+
+  _hideInputError(inputElement) {
+    this._errorElement = this._form.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.remove(this._inputErrorClass);
+    this._errorElement.textContent = "";
+  }
+
+  _resetValidation() {
+    this._inputList = Array.from(
+      this._form.parentNode.querySelectorAll(this._inputSelector)
+    );
+    this._inputList.forEach((input) => {
+      this._hideInputError(input);
+    });
+    this._toggleButtonState();
+  }
+
+  _setEventListeners() {
+    this._inputList = Array.from(
+      this._form.querySelectorAll(this._inputSelector)
+    );
+    this._buttonElement = this._form.querySelector(this._submitButtonSelector);
+    this._toggleButtonState();
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+}
+export { FormValidator };
---
+++ scripts/card.js
@@ -0,0 +1,46 @@
+import { openCardPopup } from "./scripts.js";
+
+class Card {
+  constructor(data, cardSelector) {
+    this._title = data.name;
+    this._image = data.link;
+    this._cardSelector = cardSelector;
+  }
+
+  _getTemplate() {
+    const cardElemnt = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+    return cardElemnt;
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+    this._setEventListeners();
+
+    this._element.querySelector(".card__title").textContent = this._title;
+    this._element.querySelector(".card__img").src = this._image;
+    return this._element;
+  }
+
+  _setEventListeners() {
+    this._element.querySelector(".card__img").addEventListener("click", () => {
+      openCardPopup(this._title, this._image);
+    });
+
+    this._element
+      .querySelector(".card__like")
+      .addEventListener("click", (evt) => {
+        evt.target.classList.toggle("card__like_active");
+      });
+
+    this._element
+      .querySelector(".card__trash")
+      .addEventListener("click", (evt) => {
+        evt.target.closest(".card").remove();
+      });
+  }
+}
+
+export { Card };
